913ec7afff40334f2d0dc24d49d7115246a59cb5
thamespicha <thames.pcytssk@gmail.com>
2026-09-20 17:25:11 +0700

    t

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,9 +1,11 @@
 import { defineConfig } from "prisma/config";
 
 export default defineConfig({
-  earlyAccess: true,
   schema: "prisma/schema.prisma",
   migrations: {
     path: "prisma/migrations",
+  },
+  datasource: {
+    url: process.env.DATABASE_URL!,
   },
 });
